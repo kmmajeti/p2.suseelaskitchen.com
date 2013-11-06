3d770fdf3a4a967bfc1b3340dcf87ebd6ed952af
Krishna Murthy Majeti <kmmajeti@hotmail.com>
2013-11-06 14:07:21 -0500

    updated code to add a new post

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,9 +1,26 @@
-<form method='POST' action='/posts/p_add'>
+<!-- Print post has been added message -->
+<?php if($addAnotherPost): ?>
+		<h3 class="success">Your post has been added successfully! Would you like to add another post?</h3>
+<?php endif; ?>
 
-    <label for='content'>New Post:</label><br>
-    <textarea name='content' id='content'></textarea>
+<div class="content">
+	<!-- Form to add post -->
+	<form method='POST' id="addPost" action='/posts/p_add'>
+		<!-- Print header of the page -->
+		<h1>Write a new Post</h1>
+		<!-- Print the users first and last name -->
+		<h3><?=$user->first_name?> <?=$user->last_name?>:</h3>
+		<label for='content'>Enter the content of your new Post:</label><br>
+		<!-- Textarea to enter the content of the post -->
+		<textarea name='content' id='content' rows="5" cols="50"></textarea>
+		<div class="submitButton">
+			<input type='submit' value='Submit Post'>
+		</div>
+	</form>
+	<!-- Print the error message incase if add post failed or content is empty while posting-->
+	<?php if(isset($error)): ?>
+		<h3 class="invalid">Add Post failed! Content for new post cannot be blank.</h3>
+	<?php endif; ?>
 
-    <br><br>
-    <input type='submit' value='New post'>
+</div>
 
-</form>
