fba4c96c42891f90a3e39398661100381ba5e4a9
Krishna Murthy Majeti <kmmajeti@hotmail.com>
2013-11-06 16:14:47 -0500

    updated code to reflect the current users profile

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -25,4 +25,26 @@
 			<a href="/users/edit_profile"><input type="button" name="editprofile" value="Edit Profile"></a>
 		</div>
 	</div>
+	<!-- Code to display logged in user posts -->
+	<div class="userPosts">
+		<!-- Print the header -->
+		<h2>Here are your Posts:</h2>	
+		<?php foreach($posts as $post): ?>
+			<? if($post['user_id'] == $profile_user: ?>
+				<div class="post">
+					<!-- Print Post user's name -->
+					<h3>Posted on : <?=Time::display($post['created'])?></h3>
+					<!-- Print user's post content -->
+					<p><?=$post['content']?></p>
+					<!-- Print post created timestamp -->
+					<div class="submitButton">
+						<a href="/posts/p_delete/<?=$post['post_id']?>"><input type="button" name="deletepost" value="Delete Post"></a>
+					</div>
+				</div>
+			<? endif; ?>
+		<?php endforeach; ?>
+	</div>
+
+	
+	
 </div>
