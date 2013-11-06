67a89bc73bbd4285aaab20e200390dca91db7ce7
Krishna Murthy Majeti <kmmajeti@hotmail.com>
2013-11-06 16:04:13 -0500

    updated code to reflect the current users profile

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,21 +1,25 @@
 <div class="content">
 	<!-- Print the haeader for the profile page -->
-	<h1><?=$user->first_name?>, Welcome to <?=APP_NAME?></h1>
+	<h1><?=$profile_user['first_name']?>'s Profile</h1>
 	<div class="usersProfile">
 		<!-- Display the user profile information -->
-		<h3>Here is your Profile:</h3>
+		<p class="displayLabel">Created account on: </p>
+		<p class="contentvalue"><?=Time::display($profile_user['created'])?></p>
+		<br>
 		<p class="displayLabel">First Name: </p>
-		<p class="contentvalue"><?=$user->first_name?></p>
+		<p class="contentvalue"><?=$profile_user['first_name']?></p>
 		<br>
 		<p class="displayLabel">Last Name: </p>
-		<p class="contentvalue"><?=$user->last_name?></p>
+		<p class="contentvalue"><?=$profile_user['last_name']?></p>
 		<br>
 		<p class="displayLabel">Email: </p>
-		<p class="contentvalue"><?=$user->email?></p>
+		<p class="contentvalue"><?=$profile_user['email']?></p>
 		<br>
 		<p class="displayLabel">Location: </p>
-		<p class="contentvalue"><?=$user->location?></p>
+		<p class="contentvalue"><?=$profile_user['location']?></p>
 		<br>
+		<p class="displayLabel">Bio: </p>
+		<p class="contentvalue"><?=$profile_user['bio']?></p>
 		<br></br>
 		<div class="submitButton">
 			<a href="/users/edit_profile"><input type="button" name="editprofile" value="Edit Profile"></a>
