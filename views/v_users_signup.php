1631a01ee9907b93c0f9f2dc75c97dd390293d06
Krishna Murthy Majeti <kmmajeti@hotmail.com>
2013-11-06 14:06:58 -0500

    updated code related to users signup page

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,21 +1,58 @@
-<form method='POST' action='/users/p_signup'>
+<div class="content">
+	<!-- Form for signup page -->
+	<form method='POST' id="signUpForm" action='/users/p_signup' autocomplete="off">
+			<!-- Print the haeader of the page -->
+			<h1>Create a new <?=APP_NAME?> account</h1>
+			<!-- Show the form elements to signup -->
+			<div class="formElements">
+				<label for="first_name">First Name:</label>
+				<input id="first_name" type='text' name='first_name'>
+				<br></br>
+				<label for="last_name">Last Name:</label>
+				<input id="last_name" type='text' name='last_name'>
+				<br></br>
+				<label for="email">Email:</label>
+				<input id="email" type='email' name='email'>
+				<br></br>
+				<label for="password">Password:</label>
+				<input id="password" type='password' name='password'>
+				<br></br>
+				<div class="submitButton">
+					<input type='submit' value='Sign Up'>
+				</div>
+			</div>
+	</form>
 
-    First Name:<br>
-    <input type='text' name='first_name'>
-    <br><br>
+	<!-- Code to display error message  -->
+	<?php if(isset($error)): ?>
+		<h3 class="invalid">Account with the given email is already exists in the system. Please <a href="/users/login">Login </a>to the system!</h3>
+	<?php endif; ?>
 
-    Last Name:<br>
-    <input type='text' name='last_name'>
-    <br><br>
+</div>
 
-    Email:<br>
-    <input type='text' name='email'>
-    <br><br>
-
-    Password:<br>
-    <input type='password' name='password'>
-    <br><br>
-
-    <input type='submit' value='Sign up'>
-
-</form>
+<!-- Java script code for validation of input fields -->
+<script type="text/javascript">
+	$(function(){
+		$("#signUpForm").validate({
+			errorClass: "invalid",
+			rules: {
+				first_name: "required",
+				last_name: "required",
+				password: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				first_name: " Please specify your first name",
+				last_name: " Please specify your last name",
+				password: " Please specify your password to login",
+				email: {
+					required: " Please specify your email",
+					email: " email must be in the format of name@domain.com"
+				}
+			}
+		});
+	});
+</script>
