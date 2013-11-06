67950fa6bba3841b6f6a0d7084d0f873c161b621
Krishna Murthy Majeti <kmmajeti@hotmail.com>
2013-11-06 14:08:31 -0500

    updated code to show the postss from the users they are following

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -1,15 +1,15 @@
-<?php foreach($posts as $post): ?>
-
-	<article>
-
-    <h1><?=$post['first_name']?> <?=$post['last_name']?> posted:</h1>
-
-    <p><?=$post['content']?></p>
-
-    <time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
-        <?=Time::display($post['created'])?>
-    </time>
-
-	</article>
-
-<?php endforeach; ?>
+<div class="content">
+	<!-- Print the header of the page -->
+	<h1><?=$user->first_name?> - here are the posts from the users you are following:</h1>
+	<!-- Print the posts from the users they are following -->
+	<?php foreach($posts as $post): ?>
+		<div class="post">
+			<!-- Print Post user's name -->
+			<h3><?=$post['first_name']?> <?=$post['last_name']?> posted:</h3>
+			<!-- Print user's post content -->
+			<p><?=$post['content']?></p>
+			<!-- Print post created timestamp -->
+			<p><?=Time::display($post['created'])?></p>
+		</div>
+	<?php endforeach; ?>
+</div>
