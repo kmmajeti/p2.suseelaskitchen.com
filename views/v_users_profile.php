d104be049f5f87769935496e9db268dcb6b70995
Krishna Murthy Majeti <kmmajeti@hotmail.com>
2013-11-08 22:47:14 -0500

    updated to show user posts and delete action to delete his/her posts

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,10 +1,11 @@
 <div class="content">
 	<!-- Print the haeader for the profile page -->
-	<h1><?=$profile_user['first_name']?>: Profile</h1>
+	<h1>Welcome to <?=APP_NAME?>, <?=$profile_user['first_name']?></h1>
 	<div class="usersProfile">
+		<h2>Here is your Profile:</h2>
 		<!-- Display the user profile information -->
-		<p class="displayLabel">Created account on: </p>
-		<p class="contentvalue"><?=Time::display($profile_user['created'])?></p>
+		<p class="displayLabel">Account Created: </p>
+		<p class="contentvalue"><?= date('F d, Y',$profile_user['created']) ?></p>
 		<br>
 		<p class="displayLabel">First Name: </p>
 		<p class="contentvalue"><?=$profile_user['first_name']?></p>
@@ -24,5 +25,25 @@
 		<div class="submitButton">
 			<a href="/users/edit_profile"><input type="button" name="editprofile" value="Edit Profile"></a>
 		</div>
+	</div>
+	
+	<!-- Code to display logged in user posts and code to delete his/her own posts-->
+	<div class="userPosts">
+		<!-- Print the header -->
+		<h2>You posted <?=count($posts)?> status update(s):</h2>	
+		<?php foreach($posts as $post): ?>
+			<? if($post['user_id'] == $profile_user['user_id']: ?>
+				<div class="post">
+					<!-- Print post content -->
+					<h4><?=$post['content']?></h4>
+					<!-- Print post created timestamp -->
+					<p>Posted on : <?= date('F d, Y @ g:ia',$post['created']) ?></p>
+					<!-- Show Delete button to delete his/her post -->
+					<div class="submitButton">
+						<a href="/posts/p_delete/<?=$post['post_id']?>"><input type="button" name="deletepost" value="Delete Post"></a>
+					</div>
+				</div>
+			<? endif; ?>
+		<?php endforeach; ?>
 	</div>
 </div>
